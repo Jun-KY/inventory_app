a03c07fd20d06a654fcc68674d10919fb3c316aa
Youngjun Kwon <Youngjun.Kwon@students.mq.edu.au>
2025-07-21 14:21:07 +0900

    Warehouse Service and Controller done

--- src/main/java/com/jun/inventory_app/controller/WarehouseController.java
+++ src/main/java/com/jun/inventory_app/controller/WarehouseController.java
@@ -2,22 +2,41 @@ package com.jun.inventory_app.controller;
 
 import com.jun.inventory_app.dto.WarehouseDto;
 import com.jun.inventory_app.model.Warehouse;
+import com.jun.inventory_app.qeury.WarehouseSearchCondition;
 import com.jun.inventory_app.service.WarehouseService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.nio.channels.ReadableByteChannel;
+import java.util.List;
 
 @RestController @RequiredArgsConstructor @RequestMapping("/api/warehouses")
 public class WarehouseController {
     private final WarehouseService warehouseService;
 
+    @GetMapping
+    public List<Warehouse> search(WarehouseSearchCondition condition){
+        return warehouseService.search(condition);
+    }
+
+    @GetMapping ("/{id}")
+    public Warehouse getById(@PathVariable Long id){
+        return warehouseService.getById(id);
+    }
+
     @PostMapping
     public Warehouse create(@Valid @RequestBody WarehouseDto dto){
         return warehouseService.create(dto);
+    }
+
+    @PutMapping({"/{id}"})
+    public Warehouse update(@PathVariable Long id, @Valid @RequestBody WarehouseDto dto){
+        return warehouseService.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete (@PathVariable Long id){
+        warehouseService.delete(id);
     }
 }
--- src/main/java/com/jun/inventory_app/service/WarehouseService.java
+++ src/main/java/com/jun/inventory_app/service/WarehouseService.java
@@ -3,20 +3,45 @@ package com.jun.inventory_app.service;
 import com.jun.inventory_app.dto.WarehouseDto;
 import com.jun.inventory_app.model.Warehouse;
 import com.jun.inventory_app.qeury.WarehouseQueryRepository;
+import com.jun.inventory_app.qeury.WarehouseSearchCondition;
 import com.jun.inventory_app.repository.WarehouseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 @Service @RequiredArgsConstructor @Transactional
 public class WarehouseService {
     private final WarehouseRepository warehouseRepository;
-    private final WarehouseQueryRepository warehouseQueryRepository;
+    private final WarehouseQueryRepository queryRepository;
+
+    public List<Warehouse> search (WarehouseSearchCondition condition){
+        return queryRepository.search(condition);
+    }
+
+    public Warehouse getById(Long id){
+        return warehouseRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("warehouse not found id = " + id));
+    }
 
     public Warehouse create (WarehouseDto dto){
         Warehouse warehouse = new Warehouse();
         warehouse.setName(dto.getName());
         warehouse.setLocation(dto.getLocation());
         return warehouseRepository.save(warehouse);
+    }
+
+    public Warehouse update (Long id, WarehouseDto dto){
+        Warehouse warehouse = getById(id);
+        if(dto.getName() != null) warehouse.setName(dto.getName());
+        warehouse.setLocation(dto.getLocation());
+
+        return warehouseRepository.save(warehouse);
+    }
+
+    public void delete (Long id){
+        warehouseRepository.deleteById(id);
     }
 }
